86757b26847316b7addc4396734d664ddefb7426
Askhat <jam.askhat@gmail.com>
2019-09-25 23:58:21 +0600

    the solution of the problem

--- src/index.js
+++ src/index.js
@@ -38,7 +38,31 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arrWord = expr.split("**********");
+    let result = "";
+    for (let i = 0; i < arrWord.length; i++) {
+        let word = 0;
+        let wordResult = "";
+        while (word < arrWord[i].length) {
+            let a = arrWord[i].slice(word, word + 10);
+            let letter = 0;
+            let letterResult = "";
+            while (letter < a.length) {
+                let b = a.slice(letter, letter + 2);
+                if (b == "10") {
+                    letterResult += ".";
+                } else if (b == "11") {
+                    letterResult += "-";
+                }
+                letter += 2;
+            }
+            wordResult += MORSE_TABLE[letterResult];
+            word += 10;
+
+        }
+        result = result + wordResult + " ";
+    }
+    return result.trim();
 }
 
 module.exports = {
